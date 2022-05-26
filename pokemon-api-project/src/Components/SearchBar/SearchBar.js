fec75a96b4f5919cf85dd7f17fe6bdb627e880ff
Yuttapichai Paul Linglom <linglomst@gmail.com>
2022-05-26 19:39:40 +0700

    pokeapi-2

--- pokemon-api-project/src/Components/SearchBar/SearchBar.js
+++ pokemon-api-project/src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ function SearchBar({handleSubmit, setName, name}) {
   return (
     <div>
         <form onSubmit={handleSubmit}>
-        <input onChange={(event) => setName((event.target.value).toLowerCase())} value={name} type="text" className='text-center text-lg py-4 border-solid border-2 border-rose-400 rounded-md outline-none' placeholder='Search by name' />
+        <input onChange={(event) => setName((event.target.value).toLowerCase())} value={name} type="text" className='text-center justify-center items-center text-lg py-4 border-solid border-2 border-rose-400 rounded-md outline-none' placeholder='Search by name' />
         <button className='bg-rose-400 hover:bg-rose-500 px-8 py-4 mt-5 text-md rounded-full text-white uppercase font-bold'>Search</button>
         </form>
     </div>
